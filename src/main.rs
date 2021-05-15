8d1dd65db4e0ddb011572d94e41b1b3b29c98ee3
FlatheadV8 <flatheadv8@selma.de>
2021-05-16 01:58:13 +0200

    ffprobe test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use ffprobe::{ffprobe, FfProbe, Stream};
+use std::path::Path;
+
 fn main() {
-	println!("Hallo Welt!");
-
-	let i = "2";
-
-	println!("Zahl i = {}", i);
-
-	println!("Tschüss Welt!");
+	let probe = ffprobe(Path::new("nogit/video.mp4")).unwrap();
+	for (i, stream) in probe.streams.into_iter().enumerate() {
+			println!("Stream #{} codec_name: {}", i, stream.codec_name);
+	}
 }
